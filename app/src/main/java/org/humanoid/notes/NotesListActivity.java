8a3586136f7a880bdc55035ad2e7d2cec849ce1e
Jugal Mistry <jugalmistry99@gmail.com>
2019-01-13 21:06:47 +0530

    Changed Styles and colors

--- app/src/main/java/org/humanoid/notes/NotesListActivity.java
+++ app/src/main/java/org/humanoid/notes/NotesListActivity.java
@@ -1,7 +1,8 @@
 package org.humanoid.notes;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class NotesListActivity extends AppCompatActivity {
 
